5703530c4f30f794ba36ecbd1cc2bdd709502b53
Technetium <Technetium1@users.noreply.github.com>
2019-05-27 18:40:45 -0400

    Update compile.bat
    Running as admin can break if you don't cd to where the script is

--- compile/compile.bat
+++ compile/compile.bat
@@ -4,6 +4,7 @@ python -V > NUL 2> NUL
 if errorlevel 1 echo PYTHON NOT IN PATH! && PAUSE && EXIT
 python -m pip install -U pyinstaller --user
 python -m pip install -r %~dp0..\requirements.txt --user
+cd %~dp0
 pyinstaller -F --clean "..\nortbot.py"
 ECHO Copying cacert.pem
 COPY ..\cacert.pem .\dist\cacert.pem
